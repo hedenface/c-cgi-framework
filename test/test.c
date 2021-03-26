8e6d6650c3d66db7e041c0fb08da3f0271b71769
Bryan Heden <b.heden@gmail.com>
2021-03-26 12:47:15 -0500

    add conf script/makefiles, travis, gitignore, better dir structure

--- test/test.c
+++ test/test.c
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
     html(head(title("test")), "\n", body(form("attr", "test=\"pooop\"", input("attr", "type=\"text\""))));
     free_ptrs();
     */
-    setenv("QUERY_STRING", "key1=val1&key2");
+    setenv("QUERY_STRING", "key1=val1&key2", FALSE);
     get_request_vars();
     printf("%s\n", get_request_var("key1"));
     printf("%s\n", get_request_var("key2"));
